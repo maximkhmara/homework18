9bf20840a357c615050a3520807e93022f6dc1ab
Max <maxcatalina@iMac-Max.local>
2024-04-29 23:34:33 +0300

    w

--- main.js
+++ main.js
@@ -1,59 +1,19 @@
-function Human(name, gender) {
-	this.name = name;
-	this.gender = gender;
-
-	this.intro = function () {
-		console.log(`Name: ${this.name} Gender: ${this.gender}.`);
-	};
-}
-
-function Flat() {
-	this.residents = [];
-
-	this.addResident = function (human) {
-		if (human instanceof Human) {
-			this.residents.push(human);
-			return this.residents;
-		} else {
-			return 'Error';
-		}
-	};
-}
-
-function Building(maxFlats) {
-	this.flats = [];
-	this.maxFlats = maxFlats;
-
-	this.addFlat = function (flat) {
-		if (this.flats.length < this.maxFlats) {
-			this.flats.push(flat);
-		} else {
-			console.log('Cannot add new flat. Maximum number of flats reached.');
-		}
-	};
-}
-
-const person1 = new Human('John', 'male');
-const person2 = new Human('Mike', 'male');
-const person3 = new Human('Max', 'male');
-const person4 = new Human('Irena', 'female');
-
-const flat1 = new Flat();
-const flat2 = new Flat();
-flat1.addResident(person1);
-flat1.addResident(person2);
-flat2.addResident(person3);
-flat2.addResident(person4);
-
-const building1 = new Building(3);
-console.log(`Maximum number of flats in the building: ${building1.maxFlats}`);
-building1.addFlat(flat1);
-building1.addFlat(flat2);
-
-building1.flats.forEach((el) => {
-	el.residents.forEach((person) => {
-		console.log(`Residents of the building: ${person.name}`);
+fetch(
+	'http://api.openweathermap.org/data/2.5/weather?q=KYIV&units=metric&APPID=5d066958a60d315387d9492393935c19'
+)
+	.then((response) => response.json())
+	.then((data) => {
+		document.getElementById('temperature').textContent = data.main.temp;
+		document.getElementById('pressure').textContent = data.main.pressure;
+		document.getElementById('description').textContent =
+			data.weather[0].description;
+		document.getElementById('humidity').textContent = data.main.humidity;
+		document.getElementById('wind-speed').textContent = data.wind.speed;
+		document.getElementById('wind-direction').textContent = data.wind.deg;
+		document.getElementById(
+			'weather-icon'
+		).src = `http://openweathermap.org/img/w/${data.weather[0].icon}.png`;
+	})
+	.catch((error) => {
+		console.error('Error:', error);
 	});
-});
-
-console.log(building1.flats);
